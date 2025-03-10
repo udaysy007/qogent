1174c96190e7e90d4deb02c269e88f3eed8a368d
udaysy007 <udaysy007@gmail.com>
2025-03-11 00:55:43 +0530

    fix: improve editor config for Tailwind CSS syntax highlighting and add 'use client' to HeroButton

--- src/components/ui/hero-button.tsx
+++ src/components/ui/hero-button.tsx
@@ -1,5 +1,7 @@
+'use client'
+
+import React, { ElementType, ComponentPropsWithoutRef } from 'react'
 import { cn } from '@/lib/utils'
-import { ElementType, ComponentPropsWithoutRef } from 'react'
 
 interface HeroButtonProps<T extends ElementType> {
   as?: T
